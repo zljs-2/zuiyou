a7ece1d6cb9a796e6261c692f12fecb0173ed7e3
zljs <2700544146@qq.com>
2018-03-15 18:17:46 +0800

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,26 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Pat from '@/components/pat/Pat'
+import Topic from '@/components/topic/Topic'
+import Message from '@/components/message/Message'
 import Profile from '@/components/profile/Profile'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/profile',
-      name: 'Profile',
-      component: Profile
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/home',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/pat',
+            name: 'Pat',
+            component: Pat
+        },
+        {
+            path: '/topic',
+            name: 'Topic',
+            component: Topic
+        }, {
+            path: '/message',
+            name: 'Message',
+            component: Message
+        }, {
+            path: '/profile',
+            name: 'Profile',
+            component: Profile
+        }
+    ]
 })
